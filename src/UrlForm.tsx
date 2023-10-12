8e3eb0985235b70c4c123fb46f02b10c5c6a8eef
Joshua Hinman <joshuahinman@gmail.com>
2023-10-11 18:11:33 -0700

    post can be sent to db

--- src/UrlForm.tsx
+++ src/UrlForm.tsx
@@ -1,22 +1,39 @@
+import nodeFetch from 'node-fetch';
+
+const postNewUrl = async (body: {longurl: string, shorturl: string}) => {
+  const dbUrl: URL = new URL(`${process.env.REACT_APP_SERVER_URL}/newUrl` as string);
+  console.log(dbUrl);
+  const response = await fetch(dbUrl, {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: {'Content-Type': 'application/json'}
+  });
+  console.log(response);
+}
+
 function UrlForm() {
+
   const handleSubmit = (e: any) => { //TODO - figure out why type FormEvent isnt recognized
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const body = {
-      sourceUrl: formData.get('sourceUrl'),
-      shortUrl: formData.get('shortUrl')
+      longurl: formData.get('longurl') as string,
+      shorturl: formData.get('shorturl') as string,
     }
     console.log(body);
+    postNewUrl(body);
   }
+
   return (
     <form onSubmit={handleSubmit}>
+      <h2>{process.env.REACT_APP_SERVER_URL}</h2>
       <div>
-        <label htmlFor="sourceUrl">Source Url:</label>
-        <input type="text" id="sourceUrl" name="sourceUrl"/>
+        <label htmlFor="longurl">Source Url:</label>
+        <input type="text" id="longurl" name="longurl"/>
       </div>
       <div>
-        <label htmlFor="shortUrl">Short Url:</label>
-        <input type="text" id="shortUrl" name="shortUrl"/>
+        <label htmlFor="shorturl">Short Url:</label>
+        <input type="text" id="shorturl" name="shorturl"/>
       </div>
       <div>
         <button type="submit">Submit</button>
